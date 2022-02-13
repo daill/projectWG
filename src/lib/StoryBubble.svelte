cd3347aa2ac5a70729639c036de2de25d3372261
Christian Kramer <ck.daill@gmail.com>
2022-02-13 20:59:08 +0100

    transfer

--- src/lib/StoryBubble.svelte
+++ src/lib/StoryBubble.svelte
@@ -1,11 +1,24 @@
 <script>
-  let hovering = "hey I'm a bubble";
+  let childs = [];
 
+  function addChild() {
+
+  }
 </script>
 
 
 <div>
-  <textarea class="rounded-lg bg-blue-300">
-  </textarea>
-  <button class="bg-blue-300 hocus:bg-blue-600 rounded-full">+</button>
+  <div class="overflow-visible relative max-w-sm mx-auto bg-white shadow-lg ring-1 ring-black/5 rounded-xl flex items-center gap-6 dark:bg-slate-800 dark:highlight-white/5">
+    <div class="absolute -left-6 w-24 h-24 rounded-full shadow-lg">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+    </div>
+    <div class="flex flex-col py-5 pl-24">
+      <strong class="text-slate-900 text-sm font-medium dark:text-slate-200">Andrew Alfred</strong>
+      <span class="text-slate-500 text-sm font-medium dark:text-slate-400">Technical advisor</span>
+    </div>
+    <div></div>div>
+  </div>
 </div>
+
